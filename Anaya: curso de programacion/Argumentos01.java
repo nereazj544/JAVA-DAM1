6f6c9e2365d514be33cdd5dca8f2386f5985762c
Nerea ZJ <117531760+nereazj544@users.noreply.github.com>
2023-04-01 17:45:34 +0200

    Argumentos
    Añadir argumento y mostrar por pantalla lo ultimo escrito de dicho argumento.

---
+++ Anaya: curso de programacion/Argumentos01.java
@@ -0,0 +1,10 @@
+package anyaprogramacion;
+
+public class Argumentos01 {
+
+	public static void main(String[] args) {
+		System.out.println("El ultimo elemento que me has pasado es: " + args [args.length-1]);
+
+	}
+
+}
